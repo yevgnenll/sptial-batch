2c6957a3a018068000d442ea3f959e4caaf5b32d
박승권 <yevgnenll@gmail.com>
2020-09-20 22:16:14 +0900

    first commit

---
+++ src/main/java/com/geo/spatial/SpatialBatchApplication.java
@@ -0,0 +1,13 @@
+package com.geo.spatial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpatialBatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpatialBatchApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/geo/spatial/SpatialBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo.spatial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpatialBatchApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
