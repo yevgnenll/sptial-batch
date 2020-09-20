0e2f5b0422bd625f2039adc002f7c403802623d7
박승권 <yevgnenll@gmail.com>
2020-09-20 22:17:13 +0900

    batch 가 가능하도록 설정한다.

--- src/main/java/com/geo/spatial/SpatialBatchApplication.java
+++ src/main/java/com/geo/spatial/SpatialBatchApplication.java
@@ -1,9 +1,11 @@
 package com.geo.spatial;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpatialBatchApplication {
 
   public static void main(String[] args) {
